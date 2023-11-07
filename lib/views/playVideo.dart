ef29f7950ed7eb2b475514e8af2b288d258f84d1
emptyplayer07 <emptyplayer555@gmail.com>
2023-11-07 16:13:27 +0700

    commit ke 9

--- lib/views/playVideo.dart
+++ lib/views/playVideo.dart
@@ -1,7 +1,5 @@
 import 'package:appinio_video_player/appinio_video_player.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class PlayVideo extends StatefulWidget {
   final int episode;
@@ -19,20 +17,68 @@ class PlayVideo extends StatefulWidget {
 }
 
 class _PlayVideoState extends State<PlayVideo> {
-  late CustomVideoPlayer _customVideoPlayer;
-  String asetVideoPath = "path";
+  late CustomVideoPlayerController _customVideoPlayer;
+  String asetVideoPath = "images/test.mp4";
   @override
   void initState() {
     super.initState();
+    initializeVideoPlayer();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final sizeWidth = MediaQuery.of(context).size.width;
+    final sizeHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        flexibleSpace: Column(
+          children: [
+            Container(
+              width: sizeWidth,
+              color: Colors.black,
+              height: 50,
+              child: Image.asset(
+                'images/otakudesu.png',
+                fit: BoxFit.contain,
+              ),
+            ),
+            Container(
+              width: sizeWidth,
+              height: 1,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+              ),
+            ),
+            Container(
+              width: sizeWidth,
+              height: 5,
+              decoration: const BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        alignment: Alignment.center,
+        color: Colors.black,
+        child: CustomVideoPlayer(
+          customVideoPlayerController: _customVideoPlayer,
+        ),
+      ),
+    );
   }
 
   void initializeVideoPlayer() {
     VideoPlayerController _videoPlayerController;
-    _videoPlayerController = VideoPlayerController.asset(asetVideoPath);
+    _videoPlayerController = VideoPlayerController.asset(asetVideoPath)
+      ..initialize().then((value) {
+        setState(() {});
+      });
+
+    _customVideoPlayer = CustomVideoPlayerController(
+        context: context, videoPlayerController: _videoPlayerController);
   }
 }
